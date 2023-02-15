73af948f7940541e30aeecb0a936c32c709cf795
shiyivei <shiyivei@outlook.com>
2023-02-15 14:29:14 +0800

    add tracing log

--- examples/test-nexus/src/main.rs
+++ examples/test-nexus/src/main.rs
@@ -1,22 +1,45 @@
 // Design the APIs
 use std::net::SocketAddr;
 
+use color_eyre::Report;
 use nexus::{handler::get, Router};
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 async fn handler() -> &'static str {
     "<h1> Hello, World! </h>"
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Report> {
+    setup()?;
     // build application with a route
     let app = Router::new().route("/", get(handler));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on {}", addr);
-    // run application
+    info!(%addr,"Listening on: {}",addr);
+
     nexus::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
+}
+
+fn setup() -> Result<(), Report> {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "1")
+    }
+    color_eyre::install()?;
+
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info")
+    }
+
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    Ok(())
 }
